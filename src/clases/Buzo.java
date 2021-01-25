8229e3aeed73407b5112e9036b90275fb9453226
DanteGrassiHait <dante_GH@hotmail.com.ar>
2021-01-25 18:12:26 -0300

    Contenido

--- src/clases/Buzo.java
+++ src/clases/Buzo.java
@@ -1,5 +1,54 @@
 package clases;
 
-public class Buzo {
+public class Buzo extends Ropa {
 
+	private String tipo;
+	private Boolean capucha;
+	private Boolean bolsillos;
+	
+	public Buzo() {
+		super();
+		this.tipo = "";
+		this.capucha = false;
+		this.bolsillos = false;
+	}
+	
+	public Buzo(String marca, String modelo, String color, String talle, String genero, String paraEdad, String tipo, Boolean capucha, Boolean bolsillos) {
+		super(marca, modelo, color, talle, genero, paraEdad);
+		this.tipo = tipo;
+		this.capucha = capucha;
+		this.bolsillos = bolsillos;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getCapucha() {
+		return capucha;
+	}
+
+	public void setCapucha(Boolean capucha) {
+		this.capucha = capucha;
+	}
+
+	public Boolean getBolsillos() {
+		return bolsillos;
+	}
+
+	public void setBolsillos(Boolean bolsillos) {
+		this.bolsillos = bolsillos;
+	}
+
+	@Override
+	public String toString() {
+		return "Tipo: " + getTipo() + "Capucha: " + getCapucha() + "Bolsillos: " + getBolsillos();
+	}
+	
+	
+	
 }
--- src/clases/Ropa.java
+++ src/clases/Ropa.java
@@ -2,4 +2,87 @@ package clases;
 
 public abstract class Ropa {
 
+	private String marca;
+	private String modelo;
+	private String color;
+	private String talle;
+	private String genero;
+	private String paraEdad;
+	
+	public Ropa() {
+		this.marca = "";
+		this.modelo = "";
+		this.color = "";
+		this.talle = "";
+		this.genero = "";
+		this.paraEdad = "";
+	}
+	
+	public Ropa(String marca, String modelo, String color, String talle, String genero, String paraEdad) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.talle = talle;
+		this.genero = genero;
+		this.paraEdad = paraEdad;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getTalle() {
+		return talle;
+	}
+
+	public void setTalle(String talle) {
+		this.talle = talle;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getParaEdad() {
+		return paraEdad;
+	}
+
+	public void setParaEdad(String paraEdad) {
+		this.paraEdad = paraEdad;
+	}
+
+	@Override
+	public String toString() {
+		return "Marca: " + getMarca() + "Modelo: " + getModelo() + "Color: " + getColor() + "Talle: " + getTalle() + "Genero: " + getGenero() + "ParaEdad: " + getParaEdad();
+	}
+	
+	
+	
+	
+	
+	
 }
