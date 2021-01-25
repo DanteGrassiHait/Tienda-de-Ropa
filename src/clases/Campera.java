8eee5cd962cbbbffba79c0e4bca36e831a023567
DanteGrassiHait <31975704+DanteGrassiHait@users.noreply.github.com>
2021-01-25 19:32:16 -0300

    Contenido

--- src/clases/Campera.java
+++ src/clases/Campera.java
@@ -1,5 +1,54 @@
 package clases;
 
-public class Campera {
+public class Campera extends Ropa{
 
+	private String tipo;
+	private Boolean impermeable;
+	private Boolean capucha;
+	
+	public Campera() {
+		super();
+		this.tipo = "";
+		this.impermeable = false;
+		this.capucha = false;
+	}
+	
+	public Campera(String marca, String modelo, String color, String talle, String genero, String paraEdad, String tipo, Boolean impermeable, Boolean capucha) {
+		super(marca, modelo, color, talle, genero, paraEdad);
+		this.tipo = tipo;
+		this.impermeable = impermeable;
+		this.capucha = capucha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getImpermeable() {
+		return impermeable;
+	}
+
+	public void setImpermeable(Boolean impermeable) {
+		this.impermeable = impermeable;
+	}
+
+	public Boolean getCapucha() {
+		return capucha;
+	}
+
+	public void setCapucha(Boolean capucha) {
+		this.capucha = capucha;
+	}
+
+	@Override
+	public String toString() {
+		return "Tipo: " + getTipo() + "Impermeable: " + getImpermeable() + "Capucha: " + getCapucha();
+		}
+	
+	
+	
 }
--- src/clases/Pantalon.java
+++ src/clases/Pantalon.java
@@ -1,5 +1,56 @@
 package clases;
 
-public class Pantalon {
+public class Pantalon extends Ropa{
 
+	private String tipo;
+	private Boolean impermeable;
+	private String corte; // Ejemplo: clasico, chino, skinny
+
+	public Pantalon() {
+		super();
+		this.tipo = "";
+		this.impermeable = false;
+		this.corte = "";
+	}
+
+	public Pantalon(String marca, String modelo, String color, String talle, String genero, String paraEdad, String tipo, Boolean impermeable, String corte) {
+		super(marca, modelo, color, talle, genero, paraEdad);
+		this.tipo = tipo;
+		this.impermeable = impermeable;
+		this.corte = corte;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getImpermeable() {
+		return impermeable;
+	}
+
+	public void setImpermeable(Boolean impermeable) {
+		this.impermeable = impermeable;
+	}
+
+	public String getCorte() {
+		return corte;
+	}
+
+	public void setCorte(String corte) {
+		this.corte = corte;
+	}
+
+	@Override
+	public String toString() {
+		return "Tipo: " + getTipo() + "Impermeable: " + getImpermeable() + "Corte: " + getCorte();
+	}
+	
+	
+	
+	
+	
 }
