7f221e0899c9fa4acf0b182ae1e53e9e59370018
Gianni Ricciardi <gianniricciardi3779@gmail.com>
2021-02-16 12:32:38 -0300

    banana con fritas
    tu viejha

--- src/ejecutable/main.java
+++ src/ejecutable/main.java
@@ -1,28 +1,30 @@
 package ejecutable;
 
-//import BaseDeDatos.Conexion;
-//import BaseDeDatos.buzoDao;
+import BaseDeDatos.Conexion;
+import BaseDeDatos.buzoDao;
 
-//import colecciones.Inventario;
+import colecciones.Inventario;
 
 public class main {
 
 	public static void main(String[] args) 
 	{
-		/*
+		
 		//Conexion a la base de datos
-		Conexion con = new Conexion();
-		con.conectado();
+		//Conexion con = new Conexion();
+		//con.conectado();
 		//Creacion de objetos necesarios
-		buzoDao bDao = new buzoDao();
-		Inventario inventario = new Inventario();
+		//buzoDao bDao = new buzoDao();
+		//Inventario inventario = new Inventario();
 		
-		inventario = bDao.GetAll();
+		//inventario = bDao.GetAll();
 		//System.out.println(inventario.listar());
 		
-		inventario = bDao.GetBy("talle", "L");
+		//inventario = bDao.GetBy("talle", "L");
 		//System.out.println(inventario.listar());
-		*/
+		
+		//con.desconectar();
+		
 		Menu menu = new Menu();
 		menu.menuAdmin();
 		
